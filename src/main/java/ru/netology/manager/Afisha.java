4eb637a76441f19b150bd34c01a85e5f572aaf36
Shenaeva <56427199+Shenaeva@users.noreply.github.com>
2021-03-23 21:57:18 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/manager/Afisha.java
@@ -0,0 +1,44 @@
+package ru.netology.manager;
+
+import lombok.NoArgsConstructor;
+import ru.netology.domain.MovieItem;
+
+@NoArgsConstructor
+public class Afisha {
+  private MovieItem[] items = new MovieItem[0];
+  private int moviesLength = 10;
+
+  public Afisha(int moviesLength) {
+    this.moviesLength = moviesLength;
+  }
+
+  public void add(MovieItem item)  {
+    int length = items.length + 1;
+    MovieItem[] tmp = new MovieItem[length];
+    System.arraycopy(items, 0, tmp, 0, items.length);
+    int lastIndex = tmp.length - 1;
+    tmp[lastIndex] = item;
+    items = tmp;
+  }
+
+  public MovieItem[] getAll() {
+    // условие, что если items.length <= moviesLength, то запускается цикл с огрначинием items.length
+    if (items.length <= moviesLength) {
+      MovieItem[] result = new MovieItem[items.length];
+
+      for (int i = 0; i < items.length; i++) {
+        int index = items.length -i - 1;
+        result[i] = items[index];
+      }
+      return result;
+      // если условие не выполняется, то запускается цикл с ограничением moviesLength
+    } else {
+      MovieItem[] result = new MovieItem[moviesLength];
+      for (int i = 0; i < moviesLength; i++) {
+        int index = items.length - i - 1;
+        result[i] = items[index];
+      }
+      return result;
+    }
+  }
+}
